a3d7705ed419af989aa4960557ce833c2001cecd
Yusra <13CP41@GMAIL.COM>
2021-09-03 14:06:02 +0500

    Add files via upload

---
+++ Usecases/Config/config.py
@@ -0,0 +1,10 @@
+# ------------------- Paths for weights ------------------------------ #
+#CASE_PATH = ".\\pretrained_models\\haarcascade_frontalface_alt.xml"
+CASE_PATH = ".\\pretrained_models\\haarcascade_frontalface_default.xml"
+WRN_WEIGHTS_PATH = "https://github.com/Tony607/Keras_age_gender/releases/download/V1.0/weights.18-4.06.hdf5"
+WEIGHTS_PATH = 'weights.18-4.06.hdf5'
+
+#----------------------Video---------------------------------------- #
+URL = 0
+VIDEO_SAVE = 'obamaAge-and-gender.avi'
+
